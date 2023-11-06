185f270d2f38db0753199fdab864e5c570a8fcb4
PhillipRW03 <91704677+PhillipRW03@users.noreply.github.com>
2023-11-06 08:57:41 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,11 +1,27 @@
-var video = document.querySelector("#player1");
+var video;
 
-window.addEventListener("load", function() {
+
+window.addEventListener("load", function () {
 	console.log("Good job opening the window")
-
+	video = document.querySelector("#player1");
+	video.autoplay = false;
+	video.loop = false;
 });
 
-document.querySelector("#play").addEventListener("click", function() {
+document.querySelector("#play").addEventListener("click", function () {
 	console.log("Play Video");
+	video.play();
+	var volume = document.querySelector("#volume")
+	video.volume = volume;
 });
 
+document.querySelector("#pause").addEventListener("click", function () {
+	console.log("Pause Video");
+	video.pause();
+
+});
+
+document.querySelector("#slowdown").addEventListener("click", function () {
+	
+
+});
