4f7ceed3faad853399b1b821643f0582799fd497
PhillipRW03 <91704677+PhillipRW03@users.noreply.github.com>
2023-11-06 16:44:27 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -4,10 +4,12 @@ var isMuted = false;
 
 window.addEventListener("load", function () {
 	console.log("Good job opening the window")
+});
+
+document.addEventListener("DOMContentLoaded", function () {
 	video = document.querySelector("#player1");
 	video.autoplay = false;
 	video.loop = false;
-	video.load();
 
 	document.querySelector("#play").addEventListener("click", function () {
 		console.log("Play Video");
@@ -24,20 +26,20 @@ window.addEventListener("load", function () {
 	});
 
 	document.querySelector("#slower").addEventListener("click", function () {
-		video.playbackRate *= 0.9;
+		video.playbackRate -= 0.1;
 		console.log("New video speed: " + video.playbackRate);
 
 	});
 
 	document.querySelector("#faster").addEventListener("click", function () {
-		video.playbackRate /= 0.9;
+		video.playbackRate += 0.1;
 		console.log("New video speed: " + video.playbackRate);
 	});
 
 	document.querySelector("#skip").addEventListener("click", function () {
 		var newTime = video.currentTime + 10;
 
-		if (newTime <= video.duration) {
+		if (newTime < video.duration) {
 			video.currentTime = newTime;
 		} else {
 			video.currentTime = 0;
@@ -63,16 +65,16 @@ window.addEventListener("load", function () {
 		// Update the video's volume based on the slider value
 		var volumeValue = document.querySelector("#slider").value / 100; // Convert the slider value to a range between 0 and 1
 		video.volume = volumeValue;
-		document.querySelector("#volume").textContent = Math.round(volumeValue * 100) + "%";
+		document.querySelector("#volume").textContent = `${document.querySelector("#slider").value}%`;;
 	});
 
 	document.querySelector("#vintage").addEventListener("click", function () {
 		video.classList.add("oldSchool");
-		video.classList.toggle("oldSchool");
+		//video.classList.toggle("oldSchool");
 	});
 
 	document.querySelector("#orig").addEventListener("click", function () {
 		video.classList.remove("oldSchool");
 	});
-
 });
+
